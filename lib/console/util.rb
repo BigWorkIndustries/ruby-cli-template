caadc7ecda90a9ad37c3291c68023496f884ffa1
Vincil (Surge) Bishop <vbishop@surgeforward.com>
2018-08-09 13:16:43 -0500

    Added cli template

---
+++ lib/console/util.rb
@@ -0,0 +1,7 @@
+require "console/util/version"
+
+module Console
+  module Util
+    # Your code goes here...
+  end
+end
---
+++ lib/console/util/version.rb
@@ -0,0 +1,5 @@
+module Console
+  module Util
+    VERSION = "0.1.0"
+  end
+end
